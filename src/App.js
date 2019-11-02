d74fedbfbcbbe8374b750cdab4a03282994ad8b1
cterrasid <clarette.terrasi@gmail.com>
2019-11-02 23:49:42 +0100

    Create Header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
-	return <div className="App">Soy APP</div>;
+	return (
+		<div className="App">
+			<Header title="Climate App" />
+		</div>
+	);
 }
 
 export default App;
