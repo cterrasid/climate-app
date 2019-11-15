5be4c9bb4da9545a40516654e04c7362d5be460a
cterrasid <clarette.terrasi@gmail.com>
2019-11-15 23:06:06 +0100

    Create production version

---
+++ docs/precache-manifest.05fb60ed9793d1320e772159afe5dae7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce5f935616d8067699039883fd26e6ce",
+    "url": "./index.html"
+  },
+  {
+    "revision": "044584fe107b22b3b912",
+    "url": "./static/css/main.ba751ce2.chunk.css"
+  },
+  {
+    "revision": "51bd8ea552bbd1eeb268",
+    "url": "./static/js/2.394eb208.chunk.js"
+  },
+  {
+    "revision": "044584fe107b22b3b912",
+    "url": "./static/js/main.fc733789.chunk.js"
+  },
+  {
+    "revision": "30c24088b1c779960195",
+    "url": "./static/js/runtime-main.77b28c17.js"
+  }
+]);
